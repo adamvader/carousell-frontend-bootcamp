f6d6e6b0e794152fcb0ac985a85e170c294eb83f
adamvader <adamonglt@hotmail.com>
2022-10-09 21:07:01 +0800

    managed to get auth email, given_name and family_name, passed to backend

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -3,12 +3,16 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import { BACKEND_URL } from "../constants.js";
 
 const Listing = () => {
   const [listingId, setListingId] = useState();
   const [listing, setListing] = useState({});
+
+  const { isAuthenticated, getAccessTokenSilently, loginWithRedirect, user } =
+    useAuth0();
 
   useEffect(() => {
     // If there is a listingId, retrieve the listing data
@@ -36,10 +40,28 @@ const Listing = () => {
     }
   }
 
-  const handleClick = () => {
-    axios.put(`${BACKEND_URL}/listings/${listingId}`).then((response) => {
-      setListing(response.data);
+  const handleClick = async () => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+      return;
+    }
+
+    const accessToken = await getAccessTokenSilently({
+      audience: "https://carousell/api",
+      scope: "read:current_user",
     });
+
+    const response = await axios.put(
+      `${BACKEND_URL}/listings/${listingId}`,
+      { buyerEmail: user.email },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    setListing(response.data);
   };
 
   return (
